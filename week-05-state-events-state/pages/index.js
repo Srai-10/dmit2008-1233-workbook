0b21d34b07864784eb8fde076df4b6c137754fd3
Scholastica Rai <112667614+Srai-10@users.noreply.github.com>
2024-06-08 15:39:01 -0600

    new commit

--- week-05-state-events-state/pages/index.js
+++ week-05-state-events-state/pages/index.js
@@ -1,11 +1,32 @@
-import { Container, Typography } from '@mui/material';
-
+import { Box, Button, Container, Typography } from '@mui/material';
+import { useState } from 'react';
 export default function Home() {
+  let [bgColor, setbgColor] = useState('#000');
+
+  function handleClick() {
+    if (bgColor === '#000') {
+      setbgColor('#f00');
+    } else {
+    setbgColor('#000');
+    }
+  }
+
   return (
     <Container maxWidth="lg">
       <Typography variant="h1"> Events and State</Typography>
       <Box>
-        <Button onClick={() => console.log('Button clicked ...')} variant="Outlined">Click Me!</Button>
+        <Typography variant="h2">Box Background</Typography>
+        <Button onClick={handleClick} variant="outlined">
+          Click Me!
+          </Button>
+          <Box
+          sx={{
+            mt: 2,
+            height: '200px',
+            width: '200px',
+            backgroundColor: bgColor,
+          }}
+          ></Box>
       </Box>
     </Container>
   );
